cb89c869db9c264ef289e645c9647730f041406d
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-06-15 06:59:31 +0200

    enabled new menu action to open with system editor #70

--- src/init.ts
+++ src/init.ts
@@ -202,7 +202,44 @@ menuHandler.deletePath = (path) => {
     }
 };
 
+menuHandler.openWithSystemEditor = (path) => {
 
+    var command: string = null;
+
+    //http://stackoverflow.com/questions/8683895/how-do-i-determine-the-current-operating-system-with-node-js
+    if (process.platform === 'darwin') {
+        command = "open";
+    } else if (process.platform === 'win32') {
+        command = "explorer.exe"
+    } else if (process.platform === 'linux') {
+        command = "nautilus"
+    }
+
+    if (command != null) {
+        var spawn = require('child_process').spawn;
+
+        var child = spawn(command, [path], {
+            detached: true,
+            shell: false,
+            // cwd: childCwd
+        });
+        child.unref();
+
+        child.stdout.on('data', function (data: any) {
+            console.log('stdout: ' + data);
+
+        });
+        child.stderr.on('data', function (data: any) {
+            console.log('stderr: ' + data);
+
+        });
+        child.on('close', function (code: any) {
+            console.log('closing code: ' + code);
+
+        });
+
+    }
+};
 
 
 Menus.configureIntoCpsMenu();
