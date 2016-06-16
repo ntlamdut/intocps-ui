8db49b3ab53fa4fd771a8a0719d37622842afc48
Ken Pierce <kgpierce@gmail.com>
2016-06-16 17:35:48 +0100

    Messing with buttons

--- src/dse/dse.ts
+++ src/dse/dse.ts
@@ -18,8 +18,36 @@ export class DseController extends IViewController {
       // process configuration source here
       // as a first step, we recommend extracting the JSON data into a
       // type-safe map
-      var divProgress = <HTMLInputElement>document.getElementById("message");
+      var divProgress = <HTMLInputElement>document.getElementById("blah");
       divProgress.innerText = "Coming even sooner than you think";
+    }
+
+    public doAThing(){
+        var blah = <HTMLInputElement>document.getElementById("blah");
+        //blah.innerText = "Blah";
+        var blahStatus = document.createElement("div");
+        blahStatus.className = "alert alert-success";
+        blahStatus.innerHTML = "Blah blah";
+        blah.appendChild(blahStatus);       
+    }
+
+    public testChildProcess(){
+      var spawn = require('child_process').spawn;  
+      var child = spawn("python", ["--version"], {
+            detached: true,
+            shell: false,
+            // cwd: childCwd
+      });
+      child.unref();
+
+      child.stdout.on('data', function (data: any) {
+      console.log('stdout: ' + data);
+      //Here is where the output goes
+      var blah = <HTMLInputElement>document.getElementById("blah");
+      let m = document.createElement("span");
+      m.innerText = data + "";
+      blah.appendChild(m);
+    }); 
     }
 
 }
