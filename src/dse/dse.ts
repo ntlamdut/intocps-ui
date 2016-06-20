f38005f3303ad0b1e1fd20cb60a1f37530f42c05
Ken Pierce <kgpierce@gmail.com>
2016-06-20 17:59:50 +0100

    Button to run a python script in DSE pane.

--- src/dse/dse.ts
+++ src/dse/dse.ts
@@ -1,8 +1,11 @@
 import {IntoCpsApp} from "../IntoCpsApp"
+import {SettingKeys} from "../settings/SettingKeys";
 import {SourceDom} from "../sourceDom"
-import {ViewController} from "../iViewController"
+import {IViewController} from "../iViewController"
 
-export class DseController extends ViewController {
+import Path = require('path');
+
+export class DseController extends IViewController {
 
     private doNotPressButton: HTMLButtonElement;
 
@@ -22,48 +25,30 @@ export class DseController extends ViewController {
       //divProgress.innerText = "Coming even sooner than you think";
     }
 
-    public doAThing(){
-        var blah = <HTMLInputElement>document.getElementById("blah");
-        //blah.innerText = "Blah";
-        var blahStatus = document.createElement("div");
-        blahStatus.className = "alert alert-success";
-        blahStatus.innerHTML = "Blah blah";
-        blah.appendChild(blahStatus);       
-    }
-
-    public testChildProcess(){
-      var spawn = require('child_process').spawn;  
-      var child = spawn("python", ["C:\\Users\\ken\\Downloads\\pwd.py"], {
+    public launchDse(){
+        var spawn = require('child_process').spawn;
+        let installDir = IntoCpsApp.getInstance().getSettings().getValue(SettingKeys.INSTALL_TMP_DIR);
+        let scriptFile = Path.join(installDir, "dse", "scripts", "test.py"); 
+        var child = spawn("python", [scriptFile, "hello", "world"], {
             detached: true,
             shell: false,
             // cwd: childCwd
-      });
-      child.unref();
+        });
+        child.unref();
 
-      child.stdout.on('data', function (data: any) {
-          console.log('stdout: ' + data);
-          //Here is where the output goes
-          var blah = <HTMLInputElement>document.getElementById("blah");
-          let m = document.createElement("span");
-          m.innerText = data + "";
-          blah.appendChild(m);          
-      });
-      child.stderr.on('data', function (data: any) {
-          console.log('stderr: ' + data);
-          //Here is where the error output goes
-      });
-      //child.on('close', function (code: any) {
-      //    console.log('closing code: ' + code);
-      //    //Here you can get the exit code of the script
-      //});
-
-      child.stderr.on('data', function (data: any) {
-          console.log('stdout: ' + data);
-          // here is where the output goes
-          var blah = <HTMLInputElement>document.getElementById("blah");
-          let m = document.createElement("span");
-          m.innerText = data + "";
-          blah.appendChild(m);
-      }); 
-  }
+        child.stdout.on('data', function (data: any) {
+            console.log('dse/stdout: ' + data);
+            var status = <HTMLInputElement>document.getElementById("dse-status");
+            let m = document.createElement("span");
+            m.innerText = data + "";
+            status .appendChild(m);          
+        });
+        child.stderr.on('data', function (data: any) {
+            console.log('dse/stderr: ' + data);
+            var status = <HTMLInputElement>document.getElementById("dse-status");
+            let m = document.createElement("span");
+            m.innerText = data + "";
+            status.appendChild(m);  
+        });
+    }
 }
