0021aaacafa2ff70eea58c8b5ff5f9b82424ac73
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-07-08 03:37:07 +0200

    Add connections UI.

--- src/angular2-app/mm/mm-configuration.component.ts
+++ src/angular2-app/mm/mm-configuration.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, NgZone} from "@angular/core";
 import {MultiModelConfig} from "../../intocps-configurations/MultiModelConfig";
 import IntoCpsApp from "../../IntoCpsApp";
+import {Instance, ScalarVariable, CausalityType, InstanceScalarPair, isCausalityCompatible, isTypeCompatiple} from "../coe/models/Fmu";
 
 @Component({
     selector: "mm-configuration",
@@ -11,6 +12,10 @@ export class MmConfigurationComponent extends OnInit {
     path:string;
 
     private config:MultiModelConfig;
+
+    private selectedOutputInstance:Instance;
+    private selectedOutput:ScalarVariable;
+    private selectedInputInstance:Instance;
 
     constructor(private zone:NgZone) {
 
@@ -30,5 +35,30 @@ export class MmConfigurationComponent extends OnInit {
 
     addFmu() {
         throw "not implemented";
+    }
+
+    selectOutputInstance(instance:Instance) {
+        this.selectedOutputInstance = instance;
+        this.selectedOutput = null;
+        this.selectedInputInstance = null;
+    }
+
+    selectOutput(variable:ScalarVariable) {
+        this.selectedOutput = variable;
+        this.selectedInputInstance = null;
+    }
+
+    selectInputInstance(instance:Instance) {
+        this.selectedInputInstance = instance;
+    }
+
+    getOutputs() {
+        return this.selectedOutputInstance.fmu.scalarVariables
+            .filter(variable => isCausalityCompatible(variable.causality, CausalityType.Output));
+    }
+
+    getInputs() {
+        return this.selectedInputInstance.fmu.scalarVariables
+            .filter(element => isCausalityCompatible(element.causality, CausalityType.Input) && isTypeCompatiple(element.type, this.selectedOutput.type));
     }
 }
