a4b44eeec1ce73eea593c18aa54c318af874466f
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-07-08 02:09:09 +0200

    Add UI for FMUs and instances.

--- src/angular2-app/mm/mm-configuration.component.ts
+++ src/angular2-app/mm/mm-configuration.component.ts
@@ -1,10 +1,34 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit, NgZone} from "@angular/core";
+import {MultiModelConfig} from "../../intocps-configurations/MultiModelConfig";
+import IntoCpsApp from "../../IntoCpsApp";
 
 @Component({
     selector: "mm-configuration",
     templateUrl: "./angular2-app/mm/mm-configuration.component.html"
 })
-export class MmConfigurationComponent {
+export class MmConfigurationComponent extends OnInit {
     @Input()
     path:string;
+
+    private config:MultiModelConfig;
+
+    constructor(private zone:NgZone) {
+
+    }
+
+    ngOnInit() {
+        let project = IntoCpsApp.getInstance().getActiveProject();
+
+        MultiModelConfig
+            .parse(this.path, project.getFmusPath())
+            .then(config => this.zone.run(() => this.config = config));
+    }
+
+    onSubmit() {
+        this.config.save();
+    }
+
+    addFmu() {
+        throw "not implemented";
+    }
 }
--- src/angular2-app/mm/mm-page.component.ts
+++ src/angular2-app/mm/mm-page.component.ts
@@ -1,7 +1,11 @@
 import {Component, Input} from "@angular/core";
+import {MmConfigurationComponent} from "./mm-configuration.component";
 
 @Component({
     selector: "mm-page",
+    directives: [
+        MmConfigurationComponent
+    ],
     templateUrl: "./angular2-app/mm/mm-page.component.html",
 })
 export class MmPageComponent {
