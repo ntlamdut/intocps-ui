4de14a9cdee4b2f7d30f15450411c6f6af2e3c82
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-07-13 02:37:59 +0200

    Add multi-model connection overview table.

--- src/angular2-app/mm/mm-page.component.ts
+++ src/angular2-app/mm/mm-page.component.ts
@@ -1,10 +1,12 @@
 import {Component, Input} from "@angular/core";
 import {MmConfigurationComponent} from "./mm-configuration.component";
+import {MmOverviewComponent} from "./mm-overview.component";
 
 @Component({
     selector: "mm-page",
     directives: [
-        MmConfigurationComponent
+        MmConfigurationComponent,
+        MmOverviewComponent
     ],
     templateUrl: "./angular2-app/mm/mm-page.component.html",
 })
