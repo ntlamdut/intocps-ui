57449bd8f9aa8b66e81539c5cc3528e6d5a3b0c1
Ken Pierce <kgpierce@gmail.com>
2016-08-16 18:48:05 +0100

    Added simulation component to DSE

--- src/angular2-app/dse/dse-page.component.ts
+++ src/angular2-app/dse/dse-page.component.ts
@@ -1,15 +1,16 @@
 import {Component, Input} from "@angular/core";
 import {DseOverviewComponent} from "./dse-overview.component";
-import IntoCpsApp from "../../IntoCpsApp";
+import {DseSimulationComponent} from "./dse-simulation.component";
 import {PanelComponent} from "../shared/panel.component";
 
 @Component({
     selector: "dse-page",
+    templateUrl: "./angular2-app/dse/dse-page.component.html",
     directives: [
         PanelComponent,
-        DseOverviewComponent
-    ],
-    templateUrl: "./angular2-app/dse/dse-page.component.html",
+        DseOverviewComponent,
+        DseSimulationComponent
+    ]
 })
 export class DsePageComponent {
     @Input()
