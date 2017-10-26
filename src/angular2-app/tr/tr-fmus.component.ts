02884e2504494e2b1545bf3a2d200f7893b6f964
joshoell <jos.hoell@twt-gmbh.de>
2017-10-26 11:01:37 +0200

    small correction on queries

--- src/angular2-app/tr/tr-fmus.component.ts
+++ src/angular2-app/tr/tr-fmus.component.ts
@@ -29,8 +29,8 @@ export class TrFUMsComponent extends TrOverviewComponent{
         this.subObjectPropertyID3 = "element.type";
 
         this.findAllMainObjects = "match(n{type:'fmu'}) return n.uri, n.path ";
-        this.findAllSubObjectsPart1 = "match(activity)<-[:TRACE{name:\"prov:wasGeneratedBy\"}]-({uri:'";
-        this.findAllSubObjectsPart2 = "'})-[:TRACE{name:\"oslc:satisfies\"}]->(element) return element.uri, element.hash, activity.time, element.type order by activity.time desc";
+        this.findAllSubObjectsPart1 = "match(activity)<-[:Trace{name:\"prov:wasGeneratedBy\"}]-({uri:'";
+        this.findAllSubObjectsPart2 = "'})-[:Trace{name:\"oslc:satisfies\"}]->(element) return element.uri, element.hash, activity.time, element.type order by activity.time desc";
         this.listSubObjectsName = "List requirements";
         this.updatemainObjects();
     }
