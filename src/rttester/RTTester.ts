383ea27ed77c141eb0296c3126a9c3d509e33d71
Florian Lapschies <florian.lapschies@verified.de>
2017-11-28 12:08:22 +0100

    TDG/MC: Set OSLC_PORT to value from settings manager.

--- src/rttester/RTTester.ts
+++ src/rttester/RTTester.ts
@@ -32,7 +32,8 @@ export class RTTester {
         env["RTT_TESTCONTEXT"] = RTTester.getProjectOfFile(path);
         env["RTTDIR"] = RTTester.rttInstallDir();
         env["RTT_OP_KEY"] = "TMS:19999:FMI";
-        env["OSLC_PORT"] = "8083";
+        env["OSLC_ENABLED"] = IntoCpsApp.getInstance().getSettings().getSetting(SettingKeys.ENABLE_TRACEABILITY);
+        env["OSLC_PORT"] = IntoCpsApp.getInstance().getSettings().getSetting(SettingKeys.TRACE_DAEMON_PORT);
         return env;
     }
 
