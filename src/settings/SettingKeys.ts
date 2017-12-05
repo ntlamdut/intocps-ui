1fbbce7a5b80b01545532201857d79737596e51d
Luis Diogo Couto <coutold@utrc.utc.com>
2017-12-05 12:59:22 +0000

    Import all files from previous repo.

--- src/settings/SettingKeys.ts
+++ src/settings/SettingKeys.ts
@@ -1,4 +1,34 @@
-// Module contaiing valid setting keys
+/*
+ * This file is part of the INTO-CPS toolchain.
+ *
+ * Copyright (c) 2017-CurrentYear, INTO-CPS Association,
+ * c/o Professor Peter Gorm Larsen, Department of Engineering
+ * Finlandsgade 22, 8200 Aarhus N.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3 LICENSE OR
+ * THIS INTO-CPS ASSOCIATION PUBLIC LICENSE VERSION 1.0.
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ * RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GPL 
+ * VERSION 3, ACCORDING TO RECIPIENTS CHOICE.
+ *
+ * The INTO-CPS toolchain  and the INTO-CPS Association Public License 
+ * are obtained from the INTO-CPS Association, either from the above address,
+ * from the URLs: http://www.into-cps.org, and in the INTO-CPS toolchain distribution.
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH IN THE
+ * BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS OF
+ * THE INTO-CPS ASSOCIATION.
+ *
+ * See the full INTO-CPS Association Public License conditions for more details.
+ *
+ * See the CONTRIBUTORS file for author and contributor information. 
+ */
+
 export namespace SettingKeys {
     export var DEVELOPMENT_MODE = "development_mode";
     export var ACTIVE_PROJECT = "active_project";
@@ -27,10 +57,10 @@ export namespace SettingKeys {
     DEFAULT_VALUES[RTTESTER_MBT_INSTALL_DIR] = "C:/opt/rtt-mbt";
     DEFAULT_VALUES[RTTESTER_RTTUI] = "C:/Program Files (x86)/Verified/RTTUI3/bin/rttui3.exe";
     DEFAULT_VALUES[RTTESTER_PYTHON] = "C:/Python27/python.exe";
-    DEFAULT_VALUES[UPDATE_SITE] = "https://raw.githubusercontent.com/into-cps/into-cps.github.io/master/download/";
-    DEFAULT_VALUES[DEV_UPDATE_SITE] = "https://raw.githubusercontent.com/into-cps/into-cps.github.io/development/download/";
-    DEFAULT_VALUES[EXAMPLE_REPO] = "https://raw.githubusercontent.com/into-cps/into-cps.github.io/master/examples/examples.json";
-    DEFAULT_VALUES[DEV_EXAMPLE_REPO] = "https://raw.githubusercontent.com/into-cps/into-cps.github.io/examples-dev/examples/examples.json";
+    DEFAULT_VALUES[UPDATE_SITE] = "https://raw.githubusercontent.com/INTO-CPS-Association/INTO-CPS-Association.github.io/master/download/";
+    DEFAULT_VALUES[DEV_UPDATE_SITE] = "https://raw.githubusercontent.com/INTO-CPS-Association/INTO-CPS-Association.github.io/development/download/";
+    DEFAULT_VALUES[EXAMPLE_REPO] = "https://raw.githubusercontent.com/INTO-CPS-Association/INTO-CPS-Association.github.io/master/examples/examples.json";
+    DEFAULT_VALUES[DEV_EXAMPLE_REPO] = "https://raw.githubusercontent.com/INTO-CPS-Association/INTO-CPS-Association.github.io/examples-dev/examples/examples.json";
     DEFAULT_VALUES[DEVELOPMENT_MODE] = false;
     DEFAULT_VALUES[COE_URL] = "localhost:8082";
     DEFAULT_VALUES[TRACE_DAEMON_PORT] = "8083";
